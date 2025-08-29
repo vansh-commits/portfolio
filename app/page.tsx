006b31876771d83a60cc3ce4a55684b415ab7271
vansh-commits <vansh.malhotra439@hotmail.com>
2025-08-30 00:53:12 +0530

    Fix import paths for Vercel deployment - use @ alias instead of relative paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Navbar } from "../components/Navbar";
-import { Hero } from "../components/Hero";
-import { About } from "../components/About";
-import { TechStack } from "../components/TechStack";
-import { Projects } from "../components/Projects";
-import { Footer } from "../components/Footer";
+import { Navbar } from "@/components/Navbar";
+import { Hero } from "@/components/Hero";
+import { About } from "@/components/About";
+import { TechStack } from "@/components/TechStack";
+import { Projects } from "@/components/Projects";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
